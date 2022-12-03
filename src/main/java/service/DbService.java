2c9059d507e0b45558f7d5694fd4230b28f05ef4
nshankaz12 <nkazary@calstatela.edu>
2022-12-02 17:13:30 -0800

    first commit

---
+++ src/main/java/service/DbService.java
@@ -0,0 +1,47 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DbService {
+	private String url = "jdbc:postgresql://cs1.calstatela.edu:5432/cs4222s87";
+
+    private String username = "cs4222s87";
+
+    private String password = "6cyjI4iM";
+
+    private Connection connection;
+    
+    public DbService()
+    {
+        try
+        {
+            connection = DriverManager.getConnection( url, username, password );
+        }
+        catch( SQLException e )
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public void close()
+    {
+        if( connection != null )
+        {
+            try
+            {
+                connection.close();
+            }
+            catch( SQLException e )
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
